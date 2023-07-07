508f2a07c13c7243e85210d59485af2f06df903c
Theo Vora <theo.vora@gmail.com>
2023-07-07 10:52:01 -0500

    setup tests

---
+++ Core Data DumpTests/UserSettingsManagerTests.swift
@@ -0,0 +1,14 @@
+//
+//  UserSettingsManagerTests.swift
+//  Core Data Dump
+//
+//  Created by Theo Vora on 7/7/23.
+//
+
+import XCTest
+
+final class UserSettingsManagerTests: XCTestCase {
+    func testZero() {
+        XCTFail("Keep coffee close by.")
+    }
+}
